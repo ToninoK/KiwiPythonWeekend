88f0a619426eb8542193c657d95f5ed10ab0c2f8
ToninoK <tonino.kastelan@skole.hr>
2018-10-22 11:45:05 +0200

    Added Redis for caching data

--- arriva_scraper.py
+++ arriva_scraper.py
@@ -1,19 +1,8 @@
 from requests_html import HTMLSession
 from currency_converter import CurrencyConverter
-from dataclasses import dataclass
+from cacheControl import CacheController, Journey
 
 c = CurrencyConverter()
-
-@dataclass
-class Journey:
-    source:str = None
-    destination:str = None
-    date:str = None
-    departure:str = None
-    arrival:str = None
-    duration:str = None
-    price:float = None
-    carrier:str = None
 
 class ArrivaScraper:
     def __init__(self):
@@ -28,6 +17,7 @@ class ArrivaScraper:
             "ticket-type": "oneway",
         }
         self.html = None
+        self.cacher = CacheController()
 
     def run(self, source, destination, date):
         search = [source, destination, date]
@@ -35,18 +25,23 @@ class ArrivaScraper:
         self.data["search-to"] = search[1]
         self.data["search-datetime"] = search[2]
 
+        cached_data = self.cacher.getJourneys(search)
+
+        if cached_data:
+            return cached_data
+
         self.html = self.session.post(self.url, self.data).html
 
-        return self.parse_data(search)
+        return self.parseData(search)
 
-    def parse_data(self, search):
-        dep = self.fetch_departures()
-        arr = self.fetch_arrivals()
-        dur = self.fetch_duration()
-        pr = self.fetch_prices()
-        carr = self.fetch_carrier()
+    def parseData(self, search):
+        dep = self.fetchDepartures()
+        arr = self.fetchArrivals()
+        dur = self.fetchDurations()
+        pr = self.fetchPrices()
+        carr = self.fetchCarriers()
 
-        return [
+        journeys = [
             Journey(
                 source=search[0], 
                 destination=search[1],
@@ -58,29 +53,31 @@ class ArrivaScraper:
                 carrier=carr[i]
             ) for i in range(len(dep))
         ]
+        
+        return self.cacher.cacheJourneys(journeys, search)
 
-    def fetch_departures(self):
+    def fetchDepartures(self):
         return[
             dep.find('strong')[0].text.split('-')[0][:-1] for dep in self.html.find('.vrijeme-top')
         ]
     
-    def fetch_arrivals(self):
+    def fetchArrivals(self):
         return[
             arr.find('strong')[0].text.split('-')[1][1:] for arr in self.html.find('.vrijeme-top')
         ]
     
-    def fetch_prices(self):
+    def fetchPrices(self):
         prices = [ pr.find('a')[0].text.split(',')[0] for pr in self.html.find('.cijena') if pr.find('a') != [] ]
         return[
             round(c.convert( int( ep ),'HRK' ), 2) for ep in prices
         ]
     
-    def fetch_duration(self):
+    def fetchDurations(self):
         return[
             dur.text[16:] for dur in self.html.find('.vrijeme-bottom')
         ]
 
-    def fetch_carrier(self):
+    def fetchCarriers(self):
         return[
             carr.text[9:] for carr in self.html.find('.prijevoznik')
         ]
---
+++ cacheControl.py
@@ -0,0 +1,61 @@
+from redis import Redis
+from slugify import slugify
+from dataclasses import dataclass
+import json
+
+host = "127.0.0.1"
+port = 3001
+
+@dataclass
+class Journey:
+    source:str = None
+    destination:str = None
+    date:str = None
+    departure:str = None
+    arrival:str = None
+    duration:str = None
+    price:float = None
+    carrier:str = None
+
+    def toDict(self):
+        return {
+            'source':self.source,
+            'destination':self.destination,
+            'date':self.date,
+            'departure':self.departure,
+            'arrival':self.arrival,
+            'duration':self.duration,
+            'price':self.price,
+            'carrier':self.carrier
+        }
+
+class CacheController:
+
+    def __init__(self):
+        self.red = Redis(host=host, port=port)
+    
+    def cacheJourneys(self, journeys, search_data):
+        journeys_name = self.createName(search_data)
+        self.red.setex(journeys_name, json.dumps({'journeys':[journey.toDict() for journey in journeys]}), 60*60)
+        return journeys
+
+    def getJourneys(self, search_data):
+        journeys_name = self.createName(search_data)
+        journeys = self.red.get(journeys_name)
+        if journeys:
+            return [
+                Journey(
+                    source=journey['source'], 
+                    destination=journey['destination'],
+                    date=journey['date'],
+                    departure=journey['departure'],
+                    arrival=journey['arrival'],
+                    duration=journey['duration'],
+                    price=journey['price'],
+                    carrier=journey['carrier']
+                ) for journey in dict(json.loads(journeys))['journeys']
+            ]
+        return False
+
+    def createName(self, search_data):
+        return slugify(f"journey:{search_data[0]}_{search_data[1]}_{search_data[2]}:tonino")
